3ef5d0502829e138e48acada6b74df9a96198afc
Vlad Bistreckiy <vlad.zero07@mail.ru>
2023-05-23 19:11:02 +0500

    init

---
+++ StateMachine/State.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class State
+{
+    public abstract void Enter();
+
+    public abstract void Exit();
+
+    public virtual void Update() { }
+
+    public virtual void FixedUpdate() { }
+
+    public virtual void OnCollisionEnter(Collision collision) { }
+
+    public virtual void OnTriggerEnter(Collider collider) { }
+}
---
+++ StateMachine/StateMachine.cs
@@ -0,0 +1,19 @@
+﻿public class StateMachine
+{
+    public State CurrentState;
+
+    public void Initialize(State startState)
+    {
+        CurrentState = startState;
+        CurrentState.Enter();
+    }
+
+    public void ChangeState(State newState)
+    {
+        if (CurrentState != null)
+            CurrentState.Exit();
+
+        CurrentState = newState;
+        CurrentState.Enter();
+    }
+}
